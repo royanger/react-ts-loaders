ea91e1a6e0d1de862ce67bb19156ec8e0fee14ae
Roy Anger <roy@royanger.com>
2021-05-21 17:46:21 -0400

    Swapped from create-react-library to self rolled option

---
+++ src/lib/Loader.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './loaders.css';
+
+const Loader = (props) => {
+  return (
+    <div className='loader'>
+      This is a test of the actual component inside lib.
+    </div>
+  );
+};
+
+export default Loader;
