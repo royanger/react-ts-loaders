0c9667e5d1b934f696157efe621ff76f326fe436
Roy Anger <roy@royanger.com>
2021-05-24 19:07:25 -0400

    All loaders are handled through one parent component

--- src/components/LoaderExample.tsx
+++ src/components/LoaderExample.tsx
@@ -1,32 +1,15 @@
 import * as React from 'react'
-import {
-   DualRing,
-   Hourglass,
-   Spinner,
-   DotSpinner,
-   Pulse,
-   Ellipsis,
-} from '../lib'
+import Loader from '../lib'
 
 interface Props {
    loader: string
 }
 
-const components = {
-   dualring: DualRing,
-   hourglass: Hourglass,
-   spinner: Spinner,
-   dotspinner: DotSpinner,
-   pulse: Pulse,
-   ellipsis: Ellipsis,
-}
-
 const LoaderExample = ({ loader }: Props) => {
-   const SelectLoader = components[loader]
    return (
       <div>
          <h2>Example</h2>
-         <SelectLoader />
+         <Loader type={loader} color="#000" />
       </div>
    )
 }
--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,7 +1,38 @@
+import * as React from 'react'
+
 import DualRing from './dual-ring/DualRing'
 import Hourglass from './hourglass/Hourglass'
 import Spinner from './spinner/Spinner'
 import DotSpinner from './dot-spinner/DotSpinner'
 import Pulse from './pulse/Pulse'
 import Ellipsis from './ellipsis/Ellipsis'
-export { DualRing, Hourglass, Spinner, DotSpinner, Pulse, Ellipsis }
+import './spinner/spinner.css'
+
+const components = {
+   dualring: DualRing,
+   hourglass: Hourglass,
+   spinner: Spinner,
+   dotspinner: DotSpinner,
+   pulse: Pulse,
+   ellipsis: Ellipsis,
+}
+
+interface Props {
+   type: string
+   color: string
+}
+
+const Loader = ({ type, color }: Props) => {
+   const SelectLoader = components[type]
+
+   const cssProperties = { width: '100%', '--rts-color': { color } }
+
+   return (
+      <div className="rts-loader" style={cssProperties}>
+         <p>{type}</p>
+         <SelectLoader />
+      </div>
+   )
+}
+
+export default Loader
